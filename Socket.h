46f045d5185a3e88f2b4e01a6e6044aa902055b2
YuHuangjie <yuhj@shanghaitech.edu.cn>
2020-05-21 12:25:55 +0800

    Update new programming interface

--- Socket.h
+++ Socket.h
@@ -1,55 +1,81 @@
-//
-//  Socket.h
-//  SocketServer
-//
-//  Created by Kay Makowsky on 14.06.16.
-//  Copyright © 2016 Kay Makowsky. All rights reserved.
-//
+/*
+ * socket.h -- definitions for the socket wrapper
+ * 
+ * Copyright (C) 2020 Yu Huangjie
+ * 
+ * License placeholder
+ * 
+ */
 
 #ifndef SOCKET_H
 #define SOCKET_H
-#include <iostream>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/types.h>
+
 #include <sys/socket.h>
-#include <sys/ioctl.h>
 #include <netinet/in.h>
-#include <fcntl.h>
 #include <netdb.h>
-#include <arpa/inet.h>
-#include <vector>
-#include <sys/un.h>
-#include <errno.h>
+#include <string>
 
-using namespace std;
+/* 
+ * error codes
+ */
+#define SOCKERR_OK              0
+#define SOCKERR_FAULT          -1
+#define SOCKERR_INVALID_ADDR   -2
+#define SOCKERR_PEER_CLOSED    -3
+#define SOCKERR_NODATA         -4
 
-class Socket
+/*
+ * I/O ability
+ */
+#define SOCK_READABLE           0x01
+#define SOCK_WRITEABLE          0x02
+
+class socket_in
 {
 public:
-    int sock;
-    string address;
-    string port;
-    struct addrinfo address_info;
-    Socket();
-    Socket(int domain,int type,int protocol);
-    int bind(string ip, string port);
-    int connect(string ip, string port);
-    int listen(int max_queue);
-    Socket* accept();
-    int socket_write(string msg);
-    int socket_read(string &buf,int len);
-    int socket_safe_read(string &buf,int len,int seconds);
-    int socket_writeTo(string msg, string ip, string port);
-    int socket_readFrom(string &buf, int len, string ip, string port);
-    int socket_set_opt(int level, int optname, void* optval);
-    int socket_get_opt(int level, int optname, void* optval);
-    int set_blocking();
-    int set_non_blocking();
-    int socket_shutdown(int how);
-    void close();
-    static int select(vector<Socket> *reads, vector<Socket> *writes, vector<Socket> *exceptions,int seconds);
-    static string ipFromHostName(string hostname);
+	socket_in();
+	~socket_in();
+
+	int bind(const std::string &ip, const uint16_t port);
+	int connect(const std::string &ip, const uint16_t port);
+	int listen(int max_queue);
+	socket_in* accept();
+
+    	ssize_t write(const void *buf, const size_t sz);
+	/*
+	 * Poll the socket for available data. If no data is available before
+	 * time expires, return SOCKERR_NODATA. Otherwise, try receiving `sz`
+	 * bytes of data.
+	 */
+    	ssize_t read(void *buf, const size_t sz, int seconds);
+    	ssize_t read(void *buf, const size_t sz);
+
+	int set_opt(int level, int optname, void* optval, int len);
+	int get_opt(int level, int optname, void* optval, int &len);
+	int set_blocking(bool blocking);
+	int shutdown();
+	void close();
+	/* 
+	 * get last error 
+	 */
+	std::string get_last_error();
+	/*
+	 * Monitor the socket, waiting until it becomes ready for reading or
+	 * writing.
+	 * 
+	 * On success, select returns a combination of READ_ABILITY and 
+	 * WRITE_ABILITY or 0 if no I/O is ready to perform. Otherwise, select
+	 * return SOCKERR_FAULT on failure.
+	 */
+	int select(bool watch_read, bool watch_write, int seconds);
+
+	int getnameinfo(std::string &ip, std::string &service);
+
+private:
+	int _sock;
+	struct addrinfo _addrinfo;
+	struct sockaddr _addr;
+	std::string _last_err;
 };
-#endif
+
+#endif /* SOCKET_H */
